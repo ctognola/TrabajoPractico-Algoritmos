926ed0a1b0677d90fef56a7f8ce956f924968a88
bautistaturri <turribautista551@gmail.com>
2023-10-22 10:56:46 -0300

    Agregado de map y intendo de acomodar datos en sus respectivas columnas

--- TpFinal/src/LectorCSV.java
+++ TpFinal/src/LectorCSV.java
@@ -4,15 +4,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LectorCSV {
-    private List<String> columnas;
-    private List<List<String>> datos;
+    private Map<String, List<String>> columnas;
+    private List<String> etiquetasColumnas;
 
     public LectorCSV() {
-        columnas = new ArrayList<>();
-        datos = new ArrayList<>();
+        columnas = new HashMap<>();
+        etiquetasColumnas = new ArrayList<>();
     }
 
     public void leerCSV() {
@@ -26,29 +30,59 @@ public class LectorCSV {
 
                 if (primeraLinea) {
                     // Si es la primera línea, se asume que son las etiquetas de columna
-                    for (String dato : datosDeLinea) {
-                        columnas.add(dato);
-                        datos.add(new ArrayList<>());
+                    etiquetasColumnas = Arrays.asList("nombres", "apellidos", "edades");
+                    for (String etiqueta : etiquetasColumnas) {
+                        columnas.put(etiqueta, new ArrayList<>());
                     }
                     primeraLinea = false;
                 } else {
                     for (int i = 0; i < datosDeLinea.length; i++) {
-                        datos.get(i).add(datosDeLinea[i]);
+                        columnas.get(etiquetasColumnas.get(i)).add(datosDeLinea[i]);
                     }
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<String> getColumna(String nombreColumna) {
-        List<String> columna = new ArrayList<>();
-        int indice = columnas.indexOf(nombreColumna);
-        if (indice != -1) {
-            columna = datos.get(indice);
+
+    public void ordenarPorColumna(String nombreColumna) {
+        if (columnas.containsKey(nombreColumna)) {
+            List<String> columna = columnas.get(nombreColumna);
+            columna.sort(Comparator.naturalOrder()); // Ordenar en orden ascendente
         }
-        return columna;
+    }
+    public void visualizarDatos() {
+    	// Ordenar los datos por la columna "nombres" en orden ascendente
+    	ordenarPorColumna("nombres");
+
+    	    // Imprimir las etiquetas de columna con separadores
+    	    for (String etiquetaColumna : etiquetasColumnas) {
+    	        System.out.print(etiquetaColumna + "\t|");
+    	    }
+    	    System.out.println();
+
+    	    int numRows = etiquetasColumnas.isEmpty() ? 0 : columnas.get(etiquetasColumnas.get(0)).size();
+    	    for (int i = 0; i < numRows; i++) {
+    	        System.out.print(columnas.get("nombres").get(i) + " \t|");
+    	        // Ajustar el tamaño de la columna "apellidos"
+    	        int anchoColumnaApellidos = columnas.get("apellidos").get(i).length();
+    	        String apellidosAlineados = String.format("%1$-" + anchoColumnaApellidos + "s", columnas.get("apellidos").get(i));
+    	        System.out.print(apellidosAlineados + "\t|");
+
+    	        // Ajustar el tamaño de la columna "edades"
+    	        int anchoColumnaEdades = String.valueOf(columnas.get("edades").get(i)).length();
+    	        String edadesAlineados = String.format("%1$-" + anchoColumnaEdades + "s", columnas.get("edades").get(i));
+    	        System.out.print(edadesAlineados + "\t|");
+    	        System.out.println();
+    	    }
+    	}
+
+    public List<String> getColumna(String nombreColumna) {
+        if (columnas.containsKey(nombreColumna)) {
+            return columnas.get(nombreColumna);
+        }
+        return new ArrayList<>(); // Devolver una lista vacía si no se encuentra la columna
     }
 }
--- TpFinal/src/Principal.java
+++ TpFinal/src/Principal.java
@@ -3,23 +3,39 @@ import java.util.List;
 public class Principal {
     public static void main(String[] args) {
         // Crear una instancia de LectorCSV
-        LectorCSV lectorCSV = new LectorCSV();
-        
-        // Leer el archivo CSV
-        lectorCSV.leerCSV();
-
+    	 LectorCSV lectorCSV = new LectorCSV();
+    	 lectorCSV.ordenarPorColumna("nombre");
+         // Leer el archivo CSV
+         lectorCSV.leerCSV();
+         
+         // Visualizar los datos
+         lectorCSV.visualizarDatos();
+         
         // Obtener la columna "Nombre"
-        List<String> columnaNombre = lectorCSV.getColumna("nombre");
+        List<String> columnaNombre = lectorCSV.getColumna("nombres");
         System.out.println("Columna Nombre:");
         for (String valor : columnaNombre) {
             System.out.println(valor);
         }
 
+        
+     // Obtener la columna "Edad"
+        List<String> columnaApellidos = lectorCSV.getColumna("apellidos");
+        System.out.println("Columna Apellidos:");
+        for (String valor : columnaApellidos) {
+            System.out.println(valor);
+        }
+  
+        
+        
+        
         // Obtener la columna "Edad"
-        List<String> columnaEdad = lectorCSV.getColumna("edad");
+        List<String> columnaEdad = lectorCSV.getColumna("edades");
         System.out.println("Columna Edad:");
         for (String valor : columnaEdad) {
             System.out.println(valor);
         }
+  
+
     }
 }
