f29b79d92c66237f78c2900255e25d49d19fe300
bautistaturri <107081681+bautistaturri@users.noreply.github.com>
2023-10-22 17:13:02 -0300

    Update Principal.java
    Adaptado a cualquier tipo de columnas

--- TpFinal/src/Principal.java
+++ TpFinal/src/Principal.java
@@ -3,39 +3,20 @@ import java.util.List;
 public class Principal {
     public static void main(String[] args) {
         // Crear una instancia de LectorCSV
-    	 LectorCSV lectorCSV = new LectorCSV();
-    	 lectorCSV.ordenarPorColumna("nombre");
-         // Leer el archivo CSV
-         lectorCSV.leerCSV();
-         
-         // Visualizar los datos
-         lectorCSV.visualizarDatos();
-         
-        // Obtener la columna "Nombre"
-        List<String> columnaNombre = lectorCSV.getColumna("nombres");
-        System.out.println("Columna Nombre:");
-        for (String valor : columnaNombre) {
-            System.out.println(valor);
-        }
+        LectorCSV lectorCSV = new LectorCSV();
+        
+        // Leer el archivo CSV
+        lectorCSV.leerCSV();
 
-        
-     // Obtener la columna "Edad"
-        List<String> columnaApellidos = lectorCSV.getColumna("apellidos");
-        System.out.println("Columna Apellidos:");
-        for (String valor : columnaApellidos) {
+        // Visualizar los datos
+        lectorCSV.visualizarDatos();
+/* 
+        // Obtener cualquier columna por su nombre
+        List<String> columnaPersonalizada = lectorCSV.getColumna("nombre_de_columna_personalizada");
+        System.out.println("Columna Personalizada:");
+        for (String valor : columnaPersonalizada) {
             System.out.println(valor);
         }
-  
-        
-        
-        
-        // Obtener la columna "Edad"
-        List<String> columnaEdad = lectorCSV.getColumna("edades");
-        System.out.println("Columna Edad:");
-        for (String valor : columnaEdad) {
-            System.out.println(valor);
-        }
-  
-
+*/
     }
 }
