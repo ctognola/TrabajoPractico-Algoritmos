3ecfda0d9adfbf6e9eaf7b0707e89f91ee94f49d
bautistaturri <107081681+bautistaturri@users.noreply.github.com>
2023-10-22 17:12:29 -0300

    Update LectorCSV.java
    Cambio a codigo que lee cualquier tipo de csv

--- TpFinal/src/LectorCSV.java
+++ TpFinal/src/LectorCSV.java
@@ -1,14 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class LectorCSV {
     private Map<String, List<String>> columnas;
@@ -20,7 +20,7 @@ public class LectorCSV {
     }
 
     public void leerCSV() {
-        Path filePath = Paths.get("C:\\Users\\M\\Desktop\\TrabajoFinal\\leer.csv");
+        Path filePath = Paths.get("C:\\Users\\M\\Desktop\\TrabajoFinal\\pruebanew.csv");
         try (BufferedReader br = Files.newBufferedReader(filePath)) {
             String linea;
             boolean primeraLinea = true;
@@ -30,7 +30,7 @@ public class LectorCSV {
 
                 if (primeraLinea) {
                     // Si es la primera línea, se asume que son las etiquetas de columna
-                    etiquetasColumnas = Arrays.asList("nombres", "apellidos", "edades");
+                    etiquetasColumnas = new ArrayList<>(Arrays.asList(datosDeLinea));
                     for (String etiqueta : etiquetasColumnas) {
                         columnas.put(etiqueta, new ArrayList<>());
                     }
@@ -46,38 +46,51 @@ public class LectorCSV {
         }
     }
 
-
     public void ordenarPorColumna(String nombreColumna) {
         if (columnas.containsKey(nombreColumna)) {
             List<String> columna = columnas.get(nombreColumna);
             columna.sort(Comparator.naturalOrder()); // Ordenar en orden ascendente
         }
     }
+
     public void visualizarDatos() {
-    	// Ordenar los datos por la columna "nombres" en orden ascendente
-    	ordenarPorColumna("nombres");
-
-    	    // Imprimir las etiquetas de columna con separadores
-    	    for (String etiquetaColumna : etiquetasColumnas) {
-    	        System.out.print(etiquetaColumna + "\t|");
-    	    }
-    	    System.out.println();
-
-    	    int numRows = etiquetasColumnas.isEmpty() ? 0 : columnas.get(etiquetasColumnas.get(0)).size();
-    	    for (int i = 0; i < numRows; i++) {
-    	        System.out.print(columnas.get("nombres").get(i) + " \t|");
-    	        // Ajustar el tamaño de la columna "apellidos"
-    	        int anchoColumnaApellidos = columnas.get("apellidos").get(i).length();
-    	        String apellidosAlineados = String.format("%1$-" + anchoColumnaApellidos + "s", columnas.get("apellidos").get(i));
-    	        System.out.print(apellidosAlineados + "\t|");
-
-    	        // Ajustar el tamaño de la columna "edades"
-    	        int anchoColumnaEdades = String.valueOf(columnas.get("edades").get(i)).length();
-    	        String edadesAlineados = String.format("%1$-" + anchoColumnaEdades + "s", columnas.get("edades").get(i));
-    	        System.out.print(edadesAlineados + "\t|");
-    	        System.out.println();
-    	    }
-    	}
+        // Ordenar los datos por la primera columna en orden ascendente
+        ordenarPorColumna(etiquetasColumnas.get(0));
+    
+        // Obtener el ancho máximo de cada columna
+        Map<String, Integer> anchosColumnas = new HashMap<>();
+        for (String etiqueta : etiquetasColumnas) {
+            int anchoMaximo = etiqueta.length();
+            for (String valor : columnas.get(etiqueta)) {
+                anchoMaximo = Math.max(anchoMaximo, valor.length());
+            }
+            anchosColumnas.put(etiqueta, anchoMaximo);
+        }
+    
+        // Imprimir las etiquetas de columna con separadores y barras horizontales
+        for (String etiquetaColumna : etiquetasColumnas) {
+            System.out.printf("%-" + (anchosColumnas.get(etiquetaColumna) + 1) + "s|", etiquetaColumna);
+        }
+        System.out.println();
+    
+        for (String etiquetaColumna : etiquetasColumnas) {
+            for (int i = 0; i < anchosColumnas.get(etiquetaColumna) + 1; i++) {
+                System.out.print("-");
+            }
+            System.out.print("+");
+        }
+        System.out.println();
+    
+        int numRows = etiquetasColumnas.isEmpty() ? 0 : columnas.get(etiquetasColumnas.get(0)).size();
+        for (int i = 0; i < numRows; i++) {
+            for (String etiquetaColumna : etiquetasColumnas) {
+                String valor = columnas.get(etiquetaColumna).get(i);
+                System.out.printf("%-" + (anchosColumnas.get(etiquetaColumna) + 1) + "s|", valor);
+            }
+            System.out.println();
+        }
+    }
+    
 
     public List<String> getColumna(String nombreColumna) {
         if (columnas.containsKey(nombreColumna)) {
@@ -86,3 +99,4 @@ public class LectorCSV {
         return new ArrayList<>(); // Devolver una lista vacía si no se encuentra la columna
     }
 }
+
